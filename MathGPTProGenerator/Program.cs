e248540467ccfd4a4baa9eedbe0e6d969bfbdf89
DeniedAccessLife <89941208+DeniedAccessLife@users.noreply.github.com>
2023-12-27 06:25:17 +0300

    Add files via upload

---
+++ MathGPTProGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MathGPTProGenerator
+{
+    static class Program
+    {
+        public static async Task Main()
+        {
+            Utils.Install();
+            await Chrome.Initialization();
+
+            try
+            {
+                Utils.Copyright(Chrome.version);
+                await Chrome.Start();
+            }
+            catch (Exception ex)
+            {
+                Utils.Exception(ex.Message);
+            }
+
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
